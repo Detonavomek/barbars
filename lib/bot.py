c96be77cd4d63926e52e8f961a1fb00c7ed673b7
Anton Adamiv <detonavomek@gmail.com>
2018-02-04 21:53:00 +0200

    add requests

--- lib/bot.py
+++ lib/bot.py
@@ -1,10 +1,18 @@
-from .config import ConfigManager
+from .connector import Connector
+from .parser import Parser
 
 
 class Bot:
 
-    def __init__(self, config):
+    def __init__(self):
+        self.connector = Connector()
+        self.parser = Parser()
+        self.config = None
+
+    def load(self, config):
         self.config = config
 
-    def start(self):
-        print(ConfigManager(self.config).generate_config())
+    def auth(self):
+        auth_result = self.connector.auth(self.config)
+        self.parser.auth(auth_result)
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import click
 
-from lib.bot import Bot
+from lib.manager import BotManager
 
 
 @click.command()
 @click.option('--config', help='Path to configuration file')
 def run(config):
-    Bot(config).start()
+    BotManager(config).start()
 
 if __name__ == '__main__':
     run()
